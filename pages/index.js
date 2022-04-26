d67315468f4a7c11b734bf4e1eb0de44aa88f6a8
rados4y <49685748+rados4y@users.noreply.github.com>
2022-04-26 20:51:51 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Kalkulator Biegowy!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          W trakcie tworzenia <code>pages/index.js</code>
         </p>
       </main>
 
